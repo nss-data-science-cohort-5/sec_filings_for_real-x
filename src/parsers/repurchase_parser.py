3dabba734f6852fd3b46c169229699598babb819
Bryan Finlayson <bdfinlayson@gmail.com>
2022-05-03 23:39:23 -0500

    update regex

--- src/parsers/repurchase_parser.py
+++ src/parsers/repurchase_parser.py
@@ -5,13 +5,13 @@ from src.parsers.parser_base import ParserBase
 
 class RepurchaseParser(ParserBase):
     def pattern(self):
-        return r"(?<=\.\s)?During.*repurchased.*(?=shares).+(?=cost|price).*(?=\;\s\w?|\.\s\w)"
+        return r"During.*repurchased.*(?=shares).+(?=cost|price).*(?=\;\s\w?|\.\s\w)"
 
     def read(self, text: str):
-        match = re.match(self.pattern(), text)
+        match = re.search(self.pattern(), text)
         if bool(match):
-            start = match.span()
-            end = match.span()
+            start = match.span()[0]
+            end = match.span()[1]
             return text[start:end]
         else:
             return "N/A"
